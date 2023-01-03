8dfbc5073eeae6edc67fd6b9c38d3fd069494cd8
Levko <lbeniakh@kse.org.ua>
2023-01-03 22:06:08 +0200

    Added vector

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import pygame
 import pgzero
 import pgzrun
 
+from vector import Vector
+
+
+class Paddle:
+    def __init__(self, x, y):
+        self.position = Vector(x, y)
+
+
 WIDTH = 600
 HEIGHT = 800
 
 
 def draw():
-    pass
+    screen.fill("#123456")
 
 
 def update(dt):
---
+++ vector.py
@@ -0,0 +1,33 @@
+import math
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, other):
+        if type(other) == Vector:
+            return Vector(self.x * other.x, self.y * other.y)
+        return Vector(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        return Vector(self.x / other, self.y / other)
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def magnitude(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def normalized(self):
+        number = max(abs(self.x), abs(self.y))
+        if number == 0:
+            return Vector(0, 0)
+        return Vector(self.x / number, self.y / number)
