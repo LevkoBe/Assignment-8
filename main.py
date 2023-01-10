85437cb53bf06efc600bdabc1007aff0407b389d
Denis Pasichnyk <119937568+DenisPasichnyk@users.noreply.github.com>
2023-01-10 21:50:21 +0200

    add ball and arc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pgzrun
+import random
 
 from vector import Vector
 
@@ -11,6 +12,7 @@ class Paddle:
     def draw(self):
         x = self.position
         screen.draw.filled_rect(Rect(x, HEIGHT - H, W, H), (255, 153, 51))
+    
 
     def move(self, pos):
         p = pos[0] - W / 2
@@ -21,21 +23,45 @@ class Paddle:
             self.velocity = 0
 
 
-WIDTH = 600
-HEIGHT = 800
-W = 200
-H = 20
-paddle = Paddle((WIDTH - W) / 2, 0)
 
+class Ball:
+    def __init__(self,position:Vector): 
+        self.position = position
+        self.velocity = Vector(random.randint(-20 ,20),random.randint(10 ,60))
+
+    def draw(self):
+        screen.draw.filled_circle((self.position.x, self.position.y), 5, 'green')
+
+    def move(self, dt):
+        self.position += self.velocity * dt
+    
+    
+
+
+
+WIDTH = 600 # 600
+HEIGHT = 400 #800
+W = 100 # 200
+H =10 # 20
+
+paddle = Paddle((WIDTH - W) / 2, 0)
+ball = Ball(Vector(WIDTH / 2, HEIGHT /2))
+
+
+
+ball.x = 3
+ball.y = 30
 
 def draw():
     screen.clear()
     screen.fill("#123456")
     paddle.draw()
+    ball.draw()
+    
 
 
 def update(dt):
-    pass
+    ball.move(dt)
 
 
 def on_mouse_move(pos):
