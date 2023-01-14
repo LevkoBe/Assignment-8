b5a3974e33dea44e754203af4c6d77cf1d45bebe
Levko <lbeniakh@kse.org.ua>
2023-01-14 10:34:09 +0200

    Fixed vector subtraction

--- vector.py
+++ vector.py
@@ -10,7 +10,7 @@ class Vector:
         return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
+        return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other):
         if type(other) == Vector:
@@ -31,3 +31,9 @@ class Vector:
         if number == 0:
             return Vector(0, 0)
         return Vector(self.x / number, self.y / number)
+
+
+a = Vector(0, 3)
+b = Vector(4, 0)
+c = a + b
+print(a, b, c, a.magnitude(), b.magnitude(), c.magnitude())
